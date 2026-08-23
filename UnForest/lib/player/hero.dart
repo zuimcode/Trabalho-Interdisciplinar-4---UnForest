ccb8fc3b8c156f75cbe481c14209f7712a9b8b39
Guilherme Zuim <guilhermealmeidazuim@gmail.com>
2026-08-23 18:40:44 -0300

    Personagem adicionado com animações

--- UnForest/lib/player/hero.dart
+++ UnForest/lib/player/hero.dart
@@ -1,47 +1,41 @@
 import 'package:bonfire/bonfire.dart';
-import 'package:flutter/material.dart';
 
 class Heroi extends SimplePlayer {
-  Heroi({required Vector2 position})
+  Heroi({required super.position})
       : super(
-          position: position,
-          size: Vector2(32, 32),
+          size: Vector2(64, 64),
           speed: 250,
-          animation : SimpleDirectionAnimation(
+          animation: SimpleDirectionAnimation(
             enabledFlipX: true,
             idleRight: SpriteAnimation.load(
-              'player/idle.png',
+              'characters/player.png',
               SpriteAnimationData.sequenced(
-                amount: 4,
+                amount: 2,
                 stepTime: 0.15,
                 textureSize: Vector2(32, 32),
                 texturePosition: Vector2(0, 0),
               ),
             ),
             runRight: SpriteAnimation.load(
-              'player/run.png',
+              'characters/player.png',
               SpriteAnimationData.sequenced(
-                amount: 6,
+                amount: 8,
                 stepTime: 0.15,
                 textureSize: Vector2(32, 32),
-                texturePosition: Vector2(0, 32),
+                texturePosition: Vector2(0, 96),
               ),
             ),
-          )
-        ) {
-    // mexer nesse trem, ta dando bo
-    add(RectangleHitbox(
-      size: Vector2(20, 16),
-      position: Vector2(6, 16), 
-    ));
-  }
+          ),
+        );
 
   @override
-  void render(Canvas canvas) {
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, width, height),
-      Paint()..color = Colors.blueAccent,
+  Future<void> onLoad() {
+    add(
+      RectangleHitbox(
+        size: Vector2(20, 16),
+        position: Vector2(6, 16),
+      ),
     );
-    super.render(canvas);
+    return super.onLoad();
   }
 }
