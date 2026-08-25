72b80754394b470b55287c0d06db659bb49fe6e8
BrunoBicalho13 <brunobicalho6@gmail.com>
2026-08-25 11:59:08 -0300

    adicionar tileset vulcao

---
+++ UnForest/Assets/vulcao.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="vulcao" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="Images/maps/vulcao.png" width="2048" height="2048"/>
+</tileset>
